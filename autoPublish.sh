86f3e01b51f1f7fae6fb28ce539a45e02132f673
laiyanlong <laiyanlong@gmail.com>
2016-12-20 01:27:30 +0800

    hourly find viki duplicate content on Tue Dec 20 01:27:30 CST 2016

--- autoPublish.sh
+++ autoPublish.sh
@@ -2,7 +2,8 @@
 python ./src/resource/viki.py
 
 # Git: add and commit changes
-git commit -a -m "hourly find viki duplicate content on `date`"
+git add -A
+git commit -m "hourly find viki duplicate content on `date`"
 
 # send data to Git server
 git push origin master
